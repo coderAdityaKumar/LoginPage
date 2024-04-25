944c627351a862efd39e0efcfa826c21eb8dbb78
Aditya Kumar <asr341025@gmail.com>
2024-04-25 07:53:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ main().catch((err) => console.log(err));
 
 
 async function main() {
-  await mongoose.connect(`mongodb://127.0.0.1:27017/DemoForm`);
+  await mongoose.connect(`mongodb+srv://aditya:341025@cluster0.lqk62gs.mongodb.net/loginpage`);
   console.log("DB connected.");
 }
 
